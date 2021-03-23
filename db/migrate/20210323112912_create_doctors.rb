86b655c64308f4f30833f0c9def39a977c46fe59
Lucas da Silva <lucas.silva00@outlook.ie>
2021-03-23 15:24:57 +0000

    Normal user authentication using Devise completed, basic views and bootstrap of users forms complete

---
+++ db/migrate/20210323112912_create_doctors.rb
@@ -0,0 +1,12 @@
+class CreateDoctors < ActiveRecord::Migration[6.1]
+  def change
+    create_table :doctors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :phone
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
