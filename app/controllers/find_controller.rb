b40f44ac5974daae2d4aa549afe75fabb6fef963
Lucas da Silva <lucas.silva00@outlook.ie>
2021-03-29 17:36:52 +0100

    postgres

--- app/controllers/find_controller.rb
+++ app/controllers/find_controller.rb
@@ -5,7 +5,9 @@ class FindController < ApplicationController
 	end
 
 	def find
-		@doctors = User.where("first_name = ?", "%" + params[:q] + "%")
+		@doctors = User.where("(first_name = ? or last_name = ? or email = ?) and doctor = true", "%" + params[:q] + "%", "%" + params[:q] + "%", "%" + params[:q] + "%")
+
+		@doctors = User.where("first_name SIMILAR TO " + params[:q])
 	end
 
 
