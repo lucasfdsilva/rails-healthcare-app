e27055cb21b536529edd0e3d1b6e830840c4a45e
Lucas da Silva <lucas.silva00@outlook.ie>
2021-03-29 17:31:20 +0100

    postgres

--- app/controllers/find_controller.rb
+++ app/controllers/find_controller.rb
@@ -5,7 +5,7 @@ class FindController < ApplicationController
 	end
 
 	def find
-		@doctors = User.where("(first_name = ? or last_name = ? or email = ?) and doctor = true", "%" + params[:q] + "%", "%" + params[:q] + "%", "%" + params[:q] + "%")
+		@doctors = User.where("first_name = ?", "%" + params[:q] + "%")
 	end
 
 
